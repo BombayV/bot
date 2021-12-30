5ef5937bcd94f1363d2c1a84896a5ddc30f9ef87
Bombay <OMGmsebastian@gmail.com>
2021-12-30 12:10:19 -0500

    feat(commands/joke): new api

--- src/commands/fun/joke.js
+++ src/commands/fun/joke.js
@@ -1,14 +1,25 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const fetch = require('node-fetch');
 
+const options = {
+    method: 'GET',
+    headers: {
+        "Accept" : "application/json",
+        "Content-Type" : "application/json",
+        "User-Agent" : "https://github.com/BombayV/bot"
+    }
+};
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('joke')
 		.setDescription('Replies with a random joke.'),
 	async execute(interaction) {
-        fetch('https://icanhazdadjoke.com/')
-        .then((resp) => resp.json())
-        .then((data) => interaction.reply(`${data.setup}\n${data.delivery}`))
-        .catch((e) => console.log(`[ERROR/JOKE]: Could not load joke. Read below\n` + e))
+
+        const resp = await fetch('https://icanhazdadjoke.com/', options);
+        if (resp.ok) {
+            const jsonJoke = await resp.json()
+            await interaction.reply(`**${jsonJoke.joke}**`)
+        }
 	},
 };
