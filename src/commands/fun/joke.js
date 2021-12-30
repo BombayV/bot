fd49324e954d152b9f7d4e90311a7d72dd2aad7f
NonStopGamer <nonstopgamertwitch@gmail.com>
2021-12-30 03:56:00 -0500

    Feat(Commands): Joke Fetch Link (WIP)

--- src/commands/fun/joke.js
+++ src/commands/fun/joke.js
@@ -6,7 +6,7 @@ module.exports = {
 		.setName('joke')
 		.setDescription('Replies with a random joke.'),
 	async execute(interaction) {
-        fetch('https://v2.jokeapi.dev/joke/Any')
+        fetch('https://icanhazdadjoke.com/')
         .then((resp) => resp.json())
         .then((data) => interaction.reply(`${data.setup}\n${data.delivery}`))
         .catch((e) => console.log(`[ERROR/JOKE]: Could not load joke. Read below\n` + e))
