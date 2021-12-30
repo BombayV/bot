f1d9e343a0465c38da89c4d1308d636937d27802
Bombay <OMGmsebastian@gmail.com>
2021-12-30 02:34:35 -0500

    feat(permissions): break entire loop

--- src/utils/permissions.js
+++ src/utils/permissions.js
@@ -8,13 +8,14 @@ const Roles = require('../config.js').WhitelistRoles;
 module.exports = userRoles => {
     if (!userRoles) return false;
     let hasPerms = false;
-    for (const userRole of userRoles) {
-        for (const role of Roles) {
-            if (role == userRole) {
-                hasPerms = true;
-                break;
+    loop1:
+        for (const userRole of userRoles) {
+            for (const role of Roles) {
+                if (role == userRole) {
+                    hasPerms = true;
+                    break loop1;
+                };
             };
         };
-    };
     return hasPerms;
 }
