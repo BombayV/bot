4ba1f92c5749284c146e874e79182a71f96f353f
Bombay <72895600+BombayV@users.noreply.github.com>
2024-04-20 15:05:01 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@
  * Private data
 */
 exports.Config = {
-    "BOT_TOKEN": "Nzc3MDU1MTA4MDEzNzUyMzIw.X6929g.OlSrOIwNsYLaaeF2gQZXon9eDE8",
-    "GUILD_ID": "797613999309979668",
-    "CLIENT_ID": "777055108013752320",
-    "GITHUB_AUTH": "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",
-    "MONGO_AUTH": 'mongodb+srv://BombayV:evroot@cluster0.s4rp8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    "JOKE_AUTH": 'HCk7e0KrKN5koQQ7FzZSVg==kLI9u0WQzzFIXa3K',
+    "BOT_TOKEN": "",
+    "GUILD_ID": "",
+    "CLIENT_ID": "",
+    "GITHUB_AUTH": "",
+    "MONGO_AUTH": '',
+    "JOKE_AUTH": '',
     "STATUS": "dnd",
     "DESCRIPTION": "for bad memes...",
     "COLOR": '#FE2C54',
